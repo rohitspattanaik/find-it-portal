ad858137ad9a4e27dd9393e72b3d31a001a75201
Rohit <rohit.pattanaik@gmail.com>
2025-08-28 21:47:06 -0700

    initial commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>Admin</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
